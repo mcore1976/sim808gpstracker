ae18c50a44647e03843324caabeae4c5094c8d15
Adam Loboda <43117081+mcore1976@users.noreply.github.com>
2020-05-06 08:56:51 +0200

    Update compileatmega9.bat

--- compileatmega9.bat
+++ compileatmega9.bat
@@ -5,5 +5,6 @@ avr-gcc -mmcu=atmega328p -std=gnu99 -Wall -Os -o main9.elf main9.c -w
 avr-objcopy -j .text -j .data -O ihex main9.elf main9.hex
 avr-size --mcu=atmega328p --format=avr main9.elf
 # fuse = 62 for 1MHz clock = internal 8Meg / division 8
+# ATTENTION ! if using External XTAL 8Hz with division by 8 - replace "lfuse:w:0x7f:m" below
 avrdude -c usbasp -p m328p -U lfuse:w:0x62:m  -U flash:w:"main9.hex":a
 
